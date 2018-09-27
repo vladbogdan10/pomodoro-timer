d3d72bcf6b028ad38867b25264a05ca84f910539
Vlad Bogdan <vlad.bogdan90@gmail.com>
2018-09-27 15:11:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pomodoro Timer</h1>
+        <p>Work in progress! :)</p>
+      </div>
+    );
+  }
+}
+
+export default App;
